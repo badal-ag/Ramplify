e0a474909ea73a9ec6de9609ef614ebb3fe63c96
Badal Agarwal <badal.agarwal1369@gmail.com>
2022-05-23 18:52:34 +0530

    Second Version

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is Version 2 of Ramplify
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://bit.ly/badal-agarwal"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit my Website
         </a>
       </header>
     </div>
